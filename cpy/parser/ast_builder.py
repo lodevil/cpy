0a01dc90cf145a0369193af6900ae0abc22be576
lo <lodevil@live.cn>
2013-12-25 17:04:10 +0800

    ast:function call

--- cpy/parser/ast_builder.py
+++ cpy/parser/ast_builder.py
@@ -153,7 +153,70 @@ class ASTBuilder(object):
         return ast.Lambda(args, self.handle_test(node[-1]), *node.start)
 
     def handle_varargslist(self, node):
-        pass
+        # typedargslist: (tfpdef ['=' test] (',' tfpdef ['=' test])* [','
+        #        ['*' [tfpdef] (',' tfpdef ['=' test])* [',' '**' tfpdef] | '**' tfpdef]]
+        #      |  '*' [tfpdef] (',' tfpdef ['=' test])* [',' '**' tfpdef] | '**' tfpdef)
+        # tfpdef: NAME [':' test]
+        # varargslist: (vfpdef ['=' test] (',' vfpdef ['=' test])* [','
+        #        ['*' [vfpdef] (',' vfpdef ['=' test])* [',' '**' vfpdef] | '**' vfpdef]]
+        #      |  '*' [vfpdef] (',' vfpdef ['=' test])* [',' '**' vfpdef] | '**' vfpdef)
+        # vfpdef: NAME
+        if node[0].val == '**':
+            kwarg = node[1][0].val
+            kwargannotation = node[1][2].val if len(node[1]) == 3 else None
+            return ast.arguments(args=[], vararg=None, varargannotation=None,
+                kwonlyargs=[], kwarg=kwarg, kwargannotation=kwargannotation,
+                defaults=[], kw_defaults=[], *node[0].start)
+        elif node[0].val == '*':
+            vararg, i = node[1][0].val, 3
+            varargannotation = node[1][2].val if len(node[1]) == 3 else None
+            kwonlyargs = []
+            kw_defaults = []
+            while i < len(node) and node[i].val != '**':
+                arg = ast.arg(node[i][0].val, None)
+                if len(node[i]) == 3:
+                    arg.annotation = node[i][2].val
+                kwonlyargs.append(arg)
+                if node[i + 1].val == '=':
+                    kw_defaults.append(self.handle_test(node[i + 2]))
+                    i += 4
+                else:
+                    i += 2
+            if i < len(node) and node[i].val == '**':
+                kwarg = node[i + 1][0].val
+                kwargannotation = node[i + 1][2] if len(node[i + 1]) == 3 else None
+            else:
+                kwarg, kwargannotation = None, None
+            return ast.arguments(args=[], vararg=vararg,
+                varargannotation=varargannotation, kwonlyargs=kwonlyargs,
+                kwarg=kwarg, kwargannotation=kwargannotation,
+                defaults=[], kw_defaults=kw_defaults, *node[0].start)
+        i = 0
+        args = []
+        defaults = []
+        while i < len(node) and node[i] != token.OP:
+            arg = ast.arg(node[i][0].val, None)
+            if len(node[i]) == 3:
+                arg.annotation = node[i][2].val
+            args.append(arg)
+            if node[i + 1].val == '=':
+                defaults.append(self.handle_test(node[i + 2]))
+                i += 4
+            elif len(defaults) > 0:
+                # TODO: get line
+                raise SyntaxError('non-default argument follows default argument',
+                    ('<src>', node.start[0], node.start[1], None))
+            else:
+                i += 2
+        if i < len(node):
+            argument = self.handle_varargslist(node.subs[i:])
+            argument.args = args
+            argument.defaults = defaults
+            return argument
+        return ast.arguments(args=args, vararg=None, varargannotation=None,
+            kwonlyargs=[], kwarg=None, kwargannotation=[], defaults=defaults,
+            kw_defaults=None, *node.start)
+    handle_typedargslist = handle_varargslist
 
     def handle_expr(self, node):
         # expr: xor_expr ('|' xor_expr)*
@@ -198,15 +261,6 @@ class ASTBuilder(object):
                 atom, ast.Pow, self.handle_factor(node[-1]), *node.start)
         return atom
 
-    def handle_atom(self, node):
-        # atom: ('(' [yield_expr|testlist_comp] ')' |
-        #     '[' [testlist_comp] ']' |
-        #     '{' [dictorsetmaker] '}' |
-        #     NAME | NUMBER | STRING+ | '...' | 'None' | 'True' | 'False')
-        n = node[0]
-        if n == token.Name:
-            return ast.Name(n.val, ast.Load, *node.start)
-
     def get_trailer(self, node, atom):
         # trailer: '(' [arglist] ')' | '[' subscriptlist ']' | '.' NAME
         v = node[0].val
@@ -215,17 +269,221 @@ class ASTBuilder(object):
         elif v == '(':
             if len(node) == 2:
                 return ast.Call(atom, None, None, None, None, *node.start)
-            return self.get_call(atom, node[1])
+            args, keywords, starargs, kwargs = self.get_arglist(node[1])
+            return ast.Call(atom, args, keywords, starargs, kwargs, *node.start)
         return self.get_subscriptlist(atom, node[1])
 
-    def get_call(self, left, arglist):
-        pass
-
     def get_subscriptlist(self, left, node):
+        # subscriptlist: subscript (',' subscript)* [',']
+        # subscript: test | [test] ':' [test] [sliceop]
+        # sliceop: ':' [test]
+        if len(node) == 1:
+            sl = self.get_slice(node[0])
+            return ast.Subscript(left, sl, *node.start)
+        slices = []
+        for n in node.filter(syms.subscript):
+            slices.append(self.get_slice(n))
+        extsl = ast.ExtSlice(slices, *node.start)
+        return ast.Subscript(left, extsl, *node.start)
+
+    def get_slice(self, node):
+        # subscript: test | [test] ':' [test] [sliceop]
+        # sliceop: ':' [test]
+        if len(node) == 1:
+            if node[0] == syms.test:
+                return ast.Index(self.handle_test(node[0]), *node.start)
+            return ast.Slice(None, None, None, *node.start)
+        if node[0] == syms.test:
+            lower = self.handle_test(node[0])
+            next = 2
+        else:
+            lower, next = None, 1
+        if len(node) > 2:
+            upper, next = self.handle_test(node[next]), next + 1
+            step = None
+            if next < len(node):
+                sliceop = node[next]
+                if len(sliceop) == 2:
+                    step = self.handle_test(sliceop[1])
+            return ast.Slice(lower, upper, step, *node.start)
+        return ast.Slice(lower, None, None, *node.start)
+
+    def get_arglist(self, node):
+        # arglist: (argument ',')* (argument [',']
+        #                 |'*' test (',' argument)* [',' '**' test] 
+        #                 |'**' test)
+        # return args, keywords, starargs, kwargs
+        args, keywords, starargs, kwargs = [], [], None, None
+        i = 0
+        while i < len(node) and node[i] == syms.argument:
+            arg = self.handle_argument(node[i])
+            if isinstance(arg, ast.keyword):
+                keywords.append(arg)
+            elif len(keywords) == 0:
+                args.append(arg)
+            else:
+                raise SyntaxError('non-keyword arg after keyword arg',
+                    ('<src>', node.start[0], node.start[1], None))
+            i += 2
+        if i >= len(node):
+            pass
+        elif node[i].val == '*':
+            starargs = self.handle_test(node[i + 1])
+            i += 3
+            while i < len(node) and node[i] == syms.argument:
+                kw = self.handle_argument(node[i])
+                if not isinstance(kw, ast.keyword):
+                    raise SyntaxError(
+                        'only named arguments may follow *expression',
+                        ('<src>', node.start[0], node.start[1], None))
+                keywords.append(kw)
+                i += 2
+            if i < len(node):
+                kwargs = self.handle_test(node[i + 1])
+        else:
+            kwargs = self.handle_test(node[i + 1])
+        return args, keywords, starargs, kwargs
+
+    def handle_argument(self, node):
+        # argument: test [comp_for] | test '=' test
+        if len(node) == 1:
+            return self.handle_test(node[0])
+        elif len(node) == 3:
+            k = self.handle_test(node[0])
+            v = self.handle_test(node[2])
+            return ast.keyword(k, v)
+        return ast.GeneratorExp(self.handle_test(node[0]),
+            self.handle_comp_for(node[1]), *node.start)
+
+    def handle_comp_for(self, node):
+        # comp_for: 'for' exprlist 'in' or_test [comp_iter]
+        # comp_iter: comp_for | comp_if
+        compfor = ast.comprehension(
+            self.handle_exprlist(node[1]), self.handle_or_test(node[3]),
+            None, *node.start)
+        if len(node) == 4:
+            return [compfor]
+        if node[-1][0] == syms.comp_if:
+            tails = self.handle_comp_if(node[-1][0])
+        else:
+            tails = self.handle_comp_for(node[-1][0])
+        ifs, i = [], 0
+        while i < len(tails) and isinstance(tails[i], ast.Compare):
+            ifs.append(tails[i])
+            i += 1
+        compfor.ifs = ifs
+        return [compfor] + tails[i:]
+
+    def handle_comp_if(self, node):
+        # comp_if: 'if' test_nocond [comp_iter]
+        # comp_iter: comp_for | comp_if
+        comp = self.test_nocond(node[1])
+        if len(node) == 3:
+            if node[2][0] == syms.comp_if:
+                subs = self.handle_comp_if(node[2][0])
+            else:
+                subs = self.handle_comp_for(node[2][0])
+            return [comp] + subs
+        return [comp]
+
+    def handle_exprlist(self, node):
+        # exprlist: (expr|star_expr) (',' (expr|star_expr))* [',']
         pass
 
-    def handle_expr_stmt(self, expr_stmt):
+    def handle_test_nocond(self, node):
+        # test_nocond: or_test | lambdef_nocond
+        # lambdef_nocond: 'lambda' [varargslist] ':' test_nocond
+        if node[0] == syms.or_test:
+            return self.handle_or_test(node[0])
+        node = node[0]
+        if len(node) == 3:
+            args = ast.arguments(
+                None, None, None, None, None, None, None, None)
+        else:
+            args = self.handle_varargslist(node[1])
+        return ast.Lambda(args, self.handle_test_nocond(node[-1]), *node.start)
+
+    def handle_atom(self, node):
+        # atom: ('(' [yield_expr|testlist_comp] ')' |
+        #     '[' [testlist_comp] ']' |
+        #     '{' [dictorsetmaker] '}' |
+        #     NAME | NUMBER | STRING+ | '...' | 'None' | 'True' | 'False')
+        n = node[0]
+        if n == token.NAME:
+            return ast.Name(n.val, ast.Load, *node.start)
+        elif n == token.NUMBER:
+            return ast.Num(eval(n.val), *node.start)
+        elif n.val == '...':
+            return ast.Ellipsis(*node.start)
+        elif n == token.STRING:
+            return ast.Str(self.get_string(node.subs), *node.start)
+        elif n.val == '(':
+            if len(node) == 2:
+                return ast.Tuple(None, ast.Load, *node.start)
+            if node[1] == syms.yield_expr:
+                return self.handle_yield_expr(node[1])
+            return self.handle_testlist_comp(node[1])
+        elif n.val == '[':
+            if len(node) == 2:
+                return ast.List(None, ast.Load, *node.start)
+            return self.handle_testlist_comp(node[1])
+        else:
+            return self.handle_dictorsetmaker(node[1])
+
+    def get_string(self, nodes):
+        strs = []
+        for n in nodes:
+            strs.append(n.val.strip(n.val[0]))
+        return ''.join(strs)
+
+    def handle_yield_expr(self, node):
         pass
+
+    def handle_testlist_comp(self, node):
+        pass
+
+    def handle_dictorsetmaker(self, node):
+        pass
+
+    def handle_expr_stmt(self, node):
+        # expr_stmt: testlist_star_expr (augassign (yield_expr|testlist) |
+        #                      ('=' (yield_expr|testlist_star_expr))*)
+        # augassign: ('+=' | '-=' | '*=' | '/=' | '%=' | '&=' | '|=' | '^=' |
+        #             '<<=' | '>>=' | '**=' | '//=')
+        expr = self.handle_testlist_star_expr(node[0])
+        if len(node) == 1:
+            return  ast.Expr(expr, *node.start)
+        if node[1] == syms.augassign:
+            op = operator_map[node[1][0]]
+            if node[2] == syms.yield_expr:
+                return ast.AugAssign(
+                    expr, op, self.handle_yield_expr(node[2]), *node.start)
+            return ast.AugAssign(
+                expr, op, self.handle_testlist(node[2]), *node.start)
+        targets, i = [expr], 2
+        while i < len(node):
+            if node[i] == syms.yield_expr:
+                targets.append(self.handle_yield_expr(node[i]))
+            else:
+                targets.append(self.testlist_star_expr(node[i]))
+            i += 2
+        return ast.Assign(targets[:-1], targets[-1], *node.start)
+
+    def handle_testlist_star_expr(self, node):
+        # testlist_star_expr: (test|star_expr) (',' (test|star_expr))* [',']
+        exprs = []
+        for n in node.subs:
+            if n == syms.test:
+                exprs.append(self.handle_test(n))
+            elif n == syms.star_expr:
+                exprs.append(self.handle_star_expr(n))
+        if len(exprs) == 1 and  node[-1] != token.OP:
+            return exprs[0]
+        return ast.Tuple(exprs, ast.Store)
+
+    def handle_star_expr(self, node):
+        # star_expr: '*' expr
+        return ast.Starred(self.handle_expr(node[1]), ast.Store, *node.start)
 
     def handle_del_stmt(self, del_stmt):
         pass
